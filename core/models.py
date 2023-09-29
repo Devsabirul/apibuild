15fe78a0b85a68c8c72fd9d9b749261e6b8611d0
Shishir Islam Shanto <mdsabirulislam8089@gmail.com>
2023-09-29 16:51:48 +0600

    complate model create form frontend

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class GannonPaul(models.Model):
+	talonharrison = models.CharField(max_length=100, null=True, blank=True )
+	yvonnerosales = models.CharField(max_length=80, null=True, blank=True )
+	rubysaunders = models.CharField(max_length=100, null=True, blank=True )
+	walkerduran = models.CharField(max_length=500, null=True, blank=True )
+
+class Testmodel(models.Model):
+	test1 = models.CharField(max_length=500, null=True, blank=True )
+	test2 = models.CharField(max_length=500, null=True, blank=True )
