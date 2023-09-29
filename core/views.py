5e44da19c246be9d65820a420a5b3ad3977ee51d
Shishir Islam Shanto <mdsabirulislam8089@gmail.com>
2023-09-29 17:43:26 +0600

    complate some cahge

--- core/views.py
+++ core/views.py
@@ -2,20 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from .witemodels import *
+from django.http import JsonResponse
+
 
 def home(request):
-  return HttpResponse("<div style='text-align: center;margin-top: 50px;'><h1>The install worked successfully! Congratulations! </h1></div>")
+    return HttpResponse("<div style='text-align: center;margin-top: 50px;'><h1>The install worked successfully! Congratulations! </h1></div>")
 
 
 def createDB(request):
-  if request.method == 'POST':
-    tablename = request.POST.get('tablename')
-    fieldArry = request.POST.get('fieldArry')
-    fields = json.loads(fieldArry)
-
-    for i in fields:
-      result = ''.join(i['fieldname'].split())
-      print(result.lower())
-
-    writeModel(tablename,fields)
-  return render(request,'db/index.html')
+    if request.method == 'POST':
+        tablename = request.POST.get('tablename')
+        fieldArry = request.POST.get('fieldArry')
+        fields = json.loads(fieldArry)
+        for i in fields:
+            result = ''.join(i['fieldname'].split())
+            print(result.lower())
+        writeModel(tablename, fields)
+        response_data = {
+            'message': 'Data Table created successfully', 'status': 'success'}
+        return JsonResponse(response_data)
+    return render(request, 'db/index.html')
